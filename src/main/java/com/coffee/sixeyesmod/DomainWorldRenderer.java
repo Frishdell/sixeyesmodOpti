5b51ec5d0d3681f6fa5e5915f257052fc240676f
Frishdell <min.809re@gmail.com>
2026-02-15 14:50:18 +0400

    Update DomainWorldRenderer.java

--- src/main/java/com/coffee/sixeyesmod/DomainWorldRenderer.java
+++ src/main/java/com/coffee/sixeyesmod/DomainWorldRenderer.java
@@ -1,7 +1,6 @@
 package com.coffee.sixeyesmod;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
@@ -10,14 +9,12 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = "sixeyesmod", value = Dist.CLIENT)
 public class DomainWorldRenderer {
-
     @SubscribeEvent
     public static void onRenderWorld(RenderWorldLastEvent event) {
         Minecraft mc = Minecraft.getInstance();
-        if (mc.player == null) return;
-
-        // В 1.16.5 доступ к позиции камеры через renderDispatcher
-        ActiveRenderInfo info = mc.getEntityRenderDispatcher().camera;
-        Vector3d view = info.getPosition();
+        if (mc.player == null || mc.getEntityRenderDispatcher() == null) return;
+        // Камера для снаружи
+        Vector3d view = mc.getEntityRenderDispatcher().camera.getPosition();
+        // Внешняя оболочка купола здесь
     }
 }
