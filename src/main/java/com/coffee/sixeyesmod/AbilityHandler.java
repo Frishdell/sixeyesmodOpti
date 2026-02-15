db2946316a8e210d4e5f6090c3730418ce30f95b
Frishdell <min.809re@gmail.com>
2026-02-15 14:55:27 +0400

    Update AbilityHandler.java

--- src/main/java/com/coffee/sixeyesmod/AbilityHandler.java
+++ src/main/java/com/coffee/sixeyesmod/AbilityHandler.java
@@ -13,16 +13,15 @@ public class AbilityHandler {
     @SubscribeEvent
     public static void onEntityUpdate(LivingEvent.LivingUpdateEvent event) {
         LivingEntity entity = event.getEntityLiving();
-        // В 1.16.5 используем .world
-        if (entity instanceof MobEntity && entity.world != null) {
-            PlayerEntity closestPlayer = entity.world.getNearestPlayer(entity, 5.0D);
+        if (entity instanceof MobEntity && entity.level != null) {
+            // В 1.16.5 getNearestPlayer требует 4 параметра
+            PlayerEntity closestPlayer = entity.level.getNearestPlayer(entity.getX(), entity.getY(), entity.getZ(), 5.0D, false);
             if (closestPlayer != null) {
                 ((MobEntity) entity).setNoAI(true);
             }
         }
     }
 
-    // Методы для Networking и ModEvents
     public static void executeDomain(ServerPlayerEntity player) {}
     public static void executeInfinity(ServerPlayerEntity player) {}
     public static void checkBarrier(PlayerEntity player) {}
