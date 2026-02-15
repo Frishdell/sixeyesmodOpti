2d459bbbe169d4cd029f69700d8872e936d21ccc
Frishdell <min.809re@gmail.com>
2026-02-15 14:47:29 +0400

    Update AbilityHandler.java

--- src/main/java/com/coffee/sixeyesmod/AbilityHandler.java
+++ src/main/java/com/coffee/sixeyesmod/AbilityHandler.java
@@ -3,6 +3,7 @@ package com.coffee.sixeyesmod;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,14 +14,24 @@ public class AbilityHandler {
     @SubscribeEvent
     public static void onEntityUpdate(LivingEvent.LivingUpdateEvent event) {
         LivingEntity entity = event.getEntityLiving();
-        
-        // Логика бесконечности: если моб слишком близко к игроку
         if (entity instanceof MobEntity) {
-            PlayerEntity closestPlayer = entity.level.getNearestPlayer(entity, 5.0D);
+            // В 1.16.5 используем .world вместо .level
+            PlayerEntity closestPlayer = entity.world.getNearestPlayer(entity, 5.0D);
             if (closestPlayer != null) {
-                // ИСПРАВЛЕНО ДЛЯ 1.16.5: Кастуем в MobEntity для setNoAI
                 ((MobEntity) entity).setNoAI(true);
             }
         }
+    }
+
+    public static void executeDomain(ServerPlayerEntity player) {
+        // Логика активации территории
+    }
+
+    public static void executeInfinity(ServerPlayerEntity player) {
+        // Логика активации бесконечности
+    }
+
+    public static void checkBarrier(PlayerEntity player) {
+        // Логика проверки барьера
     }
 }
